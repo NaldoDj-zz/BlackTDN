3bb085a9ebf364afb2b3583061fe935acea7da1c
Naldo DJ <marinaldo.jesus@blacktdn.com.br>
2016-06-23 09:01:02 -0300

    2016-06-23 08:54 UTC--300 Naldo DJ (marinaldo/dot/jesus/at/blacktdn/dot/com)

---
+++ scripts/win/sql/oracle/11g/ora-drop-tables.sql
@@ -0,0 +1,43 @@
+BEGIN
+   FOR cur_rec IN (SELECT object_name, object_type
+                     FROM user_objects
+                    WHERE SUBSTR(object_name,4,3)='C40' 
+                      AND SUBSTR(object_name,1,2)<>'T_'  
+                      AND SUBSTR(object_name,1,3)<>'SX5'
+                      AND object_type IN
+                            (
+                               'TABLE'
+                              ,'VIEW'
+                              ,'PACKAGE'
+                              ,'PROCEDURE'
+                              ,'FUNCTION'
+                              ,'SEQUENCE'
+                            ))
+   LOOP
+      BEGIN
+         IF cur_rec.object_type = 'TABLE'
+         THEN
+            EXECUTE IMMEDIATE    'DROP '
+                              || cur_rec.object_type
+                              || ' "'
+                              || cur_rec.object_name
+                              || '" CASCADE CONSTRAINTS';
+         ELSE
+            EXECUTE IMMEDIATE    'DROP '
+                              || cur_rec.object_type
+                              || ' "'
+                              || cur_rec.object_name
+                              || '"';
+         END IF;
+      EXCEPTION
+         WHEN OTHERS
+         THEN
+            DBMS_OUTPUT.put_line (   'FAILED: DROP '
+                                  || cur_rec.object_type
+                                  || ' "'
+                                  || cur_rec.object_name
+                                  || '"'
+                                 );
+      END;
+   END LOOP;
+END;
